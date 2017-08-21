83525f0b4cf9ccbf3b20a359fc068e813280c5db
Sasha Sydorenko <sashokua91@gmail.com>
2017-08-21 20:21:15 +0300

    config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -74,4 +74,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # ReactJS
+  config.react.variant = :production
 end
